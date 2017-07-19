a94ba14bda571152dc2dd51c7b776419ff4f8898
Anthony Benavente <anthony@admissionpros.com>
2017-07-19 14:47:14 -0400

    Recompiled for production

---
+++ components/tabview/tabview.d.ts
@@ -0,0 +1,55 @@
+import { ElementRef, EventEmitter, AfterContentInit, QueryList } from '@angular/core';
+import { BlockableUI } from '../common/blockableui';
+export declare class TabViewNav {
+    tabs: TabPanel[];
+    orientation: string;
+    onTabClick: EventEmitter<any>;
+    onTabCloseClick: EventEmitter<any>;
+    onTabDropSuccess: EventEmitter<any>;
+    getDefaultHeaderClass(tab: TabPanel): string;
+    clickTab(event: any, tab: TabPanel): void;
+    clickClose(event: any, tab: TabPanel): void;
+    tabDrop(event: any): void;
+}
+export declare class TabPanel {
+    header: string;
+    selected: boolean;
+    disabled: boolean;
+    closable: boolean;
+    headerStyle: any;
+    headerStyleClass: string;
+    leftIcon: string;
+    rightIcon: string;
+    id: number;
+    closed: boolean;
+    lazy: boolean;
+}
+export declare class TabView implements AfterContentInit, BlockableUI {
+    el: ElementRef;
+    orientation: string;
+    style: any;
+    styleClass: string;
+    controlClose: boolean;
+    lazy: boolean;
+    tabPanels: QueryList<TabPanel>;
+    onChange: EventEmitter<any>;
+    onClose: EventEmitter<any>;
+    onTabOrderChange: EventEmitter<any>;
+    onTabInit: EventEmitter<any>;
+    initialized: boolean;
+    tabs: TabPanel[];
+    private _activeIndex;
+    constructor(el: ElementRef);
+    ngAfterContentInit(): void;
+    initTabs(): void;
+    open(event: Event, tab: TabPanel): void;
+    close(event: Event, tab: TabPanel): void;
+    closeTab(tab: TabPanel): void;
+    findSelectedTab(): TabPanel;
+    findTabIndex(tab: TabPanel): number;
+    getBlockableElement(): HTMLElement;
+    onDropSuccess(e: any): any;
+    activeIndex: number;
+}
+export declare class TabViewModule {
+}
